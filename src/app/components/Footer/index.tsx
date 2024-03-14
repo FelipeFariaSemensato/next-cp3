04f654cfeec98e6e52ddbd9544c190638bb55ca1
Felipe Faria <felipef.semensato@gmail.com>
2024-03-14 08:48:04 -0300

    Componente Header e Footer

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Todos direitos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex-auto">
-        
+        <p>Conteudo</p>
     </main>
   );
 }
